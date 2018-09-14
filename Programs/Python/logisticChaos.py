8bd845a05b70a7417bf9e62479c1afc3ee6c30a6
David M. Golembiowski <38993137+dmgolembiowski@users.noreply.github.com>
2018-09-14 09:48:25 -0400

    Update logisticChaos.py
    Improved data storage integrity as self.Data for future access

--- Programs/Python/logisticChaos.py
+++ Programs/Python/logisticChaos.py
@@ -17,27 +17,28 @@ getcontext().prec = 100000
 d = decimal.Decimal
 
 class DataPoints:
-	def __init__(self, X0, r, termCount):
-		self.X0 = d(X0)
-		self.r = d(r)
-		self.termCount = termCount
-		# self.limit = limit
-		"""self.Data = range... actually creates a list"""
-		self.Data = np.ndarray([int(1),self.X0], self.r) 
+    def __init__(self, X0, r, termCount):
+        self.X0 = X0
+        self.r = r
+        self.termCount = termCount
+        # self.limit = limit
+        """self.Data = range... actually creates a list"""
+        self.Data = [self.r, self.X0]
 
-	def graphIt(self):
-		# Yeah, not sure how to do this yet...
-		pass
+    def graphIt(self):
+    # Yeah, not sure how to do this yet...
+        pass
 
-	def generateTerms(self):
-		Output = self.X0
-		for i in range(self.termCount):
-			Output = Output*d(self.r)*(d(1) - Output)
-			self.Data.append([i,str(Output)], self.r)
-			print(self.Data)
-			i += 1
-	def exportData(self):
-		pass
+    def generateTerms(self):
+        Output = self.X0
+        for i in range(self.termCount):
+            Output = Output*(self.r)*(1 - Output)
+            self.Data.append([self.r, Output])
+            print('self.Data = ',Output)
+            print()
+
+    def exportData(self):
+        pass
 
 print('The logistic chaos equation is Xn+1 = R*Xn*(1-Xn).')
 print('The default decimal accuracy is set for one-hundred thousand decimal places.')
@@ -48,20 +49,20 @@ goAgain = True
 testCaseCounter = 0
 
 while goAgain == True:
-	rValue = input('Enter the value of R:   ')
-	rValue = str(d(rValue))
-	xNaught = input('Enter the value of X0:   ')
-	xNaught = str(d(xNaught))
-	print('How many pseudoaccurate terms would you like to generate?')
-	term_Count = input()
-	term_Count = int(term_Count)
+    rValue = input('Enter the value of R:   ')
+    rValue = float(rValue)
+    xNaught = input('Enter the value of X0:   ')
+    xNaught = float(xNaught)
+    print('How many pseudoaccurate terms would you like to generate?')
+    term_Count = input()
+    term_Count = int(term_Count)
 
-	# Create new instances of the DataPoints class
-	# for retaining data to export and analyze
-	testCase = DataPoints(xNaught, rValue, term_Count)
-	DataPoints.generateTerms(testCase)
-	testCaseCounter += 1
+    # Create new instances of the DataPoints class
+    # for retaining data to export and analyze
+    testCase = DataPoints(xNaught, rValue, term_Count)
+    DataPoints.generateTerms(testCase)
+    testCaseCounter += 1
 
-	wannaGoAgain = input('Wanna test another set?'  )
-	if wannaGoAgain == 'n':
-		sys.exit()
+    wannaGoAgain = input('Wanna test another set?'  )
+    if wannaGoAgain == 'n':
+        sys.exit()
